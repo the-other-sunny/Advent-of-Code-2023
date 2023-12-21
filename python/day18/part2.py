98774dd2ba78c6739f99ca81bf57d74884bc0112
the-other-sunny <99849452+the-other-sunny@users.noreply.github.com>
2023-12-21 02:44:41 +0100

    feat: day 18

---
+++ python/day18/part2.py
@@ -0,0 +1,86 @@
+from enum import Enum
+import re
+
+
+class Direction(Enum):
+    Right = (0, 1)
+    Down = (1, 0)
+    Left = (0, -1)
+    Up = (-1, 0)
+
+
+def get_dir_from_char(char: str) -> Direction:
+    match char:
+        case "0":
+            return Direction.Right
+        case "1":
+            return Direction.Down
+        case "2":
+            return Direction.Left
+        case "3":
+            return Direction.Up
+        case _:
+            raise RuntimeError
+
+
+def parse(input: str) -> list[tuple[Direction, int]]:
+    result = []
+
+    for m in re.finditer(
+        r"^(R|D|L|U) \d+ \(#(?P<color>[0-9a-f]{6})\)$", input, re.MULTILINE
+    ):
+        color = m["color"]
+        count = int(color[:-1], base=16)
+        dir = get_dir_from_char(color[-1])
+
+        result.append((dir, count))
+
+    return result
+
+
+def get_positions(
+    instructions: list[tuple[Direction, int]]
+) -> tuple[list[tuple[int, int]], int]:
+    positions: list[tuple[int, int]] = [(0, 0)]
+    perimeter = 0
+
+    i, j = 0, 0
+    for dir, count in instructions:
+        di, dj = dir.value
+
+        i += count * di
+        j += count * dj
+
+        positions.append((i, j))
+        perimeter += count
+
+    return positions, perimeter
+
+
+def shoelace(positions: list[tuple[int, int]]) -> float:
+    a = 0
+
+    for (x_a, y_a), (x_b, y_b) in zip(positions, positions[1:]):
+        a += (y_a + y_b) * (x_a - x_b) / 2
+
+    (x_a, y_a), (x_b, y_b) = positions[-1], positions[0]
+    a += (y_a + y_b) * (x_a - x_b) / 2
+
+    return a
+
+
+def solve(input: str) -> int:
+    instructions = parse(input)
+    positions, perimeter = get_positions(instructions)
+
+    a = abs(shoelace(positions)) + perimeter / 2 + 1
+
+    return int(a)
+
+
+if __name__ == "__main__":
+    with open("./inputs/day18.txt", encoding="utf-8") as file:
+        input = file.read().strip()
+
+    answer = solve(input)
+    print(f"{answer = }")
